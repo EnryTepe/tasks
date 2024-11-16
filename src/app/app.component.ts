4dfdc94705a0fd395ddc557ef2b26d2e8fb9cab9
EnriTepe <kique09ht@gmail.com>
2024-11-15 22:51:11 -0600

    firts commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Task } from './interfaces/task';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from './services/tasks.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task';
+
+  private _tasksService = inject(TasksService);
+
+  id: number = 0;
+
+  task: Task = {
+    id: 0,
+    title: '',
+    description: '',
+    completed: false
+  }
+
+  arr_tasks: Task[] = [];
+
+  addTask() {
+    this.task.id = this.id;
+    this._tasksService.addTask(this.task);
+    this.id++;
+    this.getTasks();
+    this.resetTask();
+  }
+
+  getTasks() {
+    this.arr_tasks = this._tasksService.getTasks();
+  }
+
+  deleteTask(id: number) {
+    this._tasksService.deleteTask(id);
+    this.getTasks();
+  }
+
+  completeTask(id: number) {
+    this._tasksService.completeTask(id);
+    this.getTasks();
+  }
+
+  resetTask() {
+    this.task = {
+      id: 0,
+      title: '',
+      description: '',
+      completed: false
+    };
+  }
 }
